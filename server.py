05c1b77d6bf7bd883f68bed38ac846922fc0f0f1
Carmine White <c.white@ahkclean.com>
2019-03-07 16:06:04 -0800

    html flask table assignment

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+Bootstrap(app)
+
+users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+
+@app.route('/')
+def index():
+        return render_template('table.html', user = users )
+
+if __name__=="__main__":
+        app.run(debug=True)
